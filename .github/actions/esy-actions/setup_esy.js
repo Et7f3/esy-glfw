cd10f4754ee3e0a1404e2cca7b3ed711230d5202
Et7f3 <cadeaudeelie@gmail.com>
2019-11-10 23:22:51 +0100

    print var

--- .github/actions/esy-actions/setup_esy.js
+++ .github/actions/esy-actions/setup_esy.js
@@ -18,6 +18,25 @@ child_process.execSync("npm install -g esy");
 
 var Core = require("@actions/core");
 var sandbox = Core.getInput("sandbox");
+
+
+console.log("string:");
+console.log(Core.getInput("string"));
+console.log(typeof(Core.getInput("string")));
+
+console.log("strng2:");
+console.log(Core.getInput("strng2"));
+console.log(typeof(Core.getInput("strng2")));
+
+console.log("arrayos:");
+console.log(Core.getInput("arrayos"));
+console.log(typeof(Core.getInput("arrayos")));
+
+console.log("array:");
+console.log(Core.getInput("array"));
+console.log(typeof(Core.getInput("array")));
+
+
 function execSync(command) {
   console.log(`Run "${command}"`);
   child_process.execSync(command);
